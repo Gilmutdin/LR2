df7a0def270b61590091ad080a014fd2b8507b4e
Gilmutdinov <edgilm@mail.ru>
2023-10-09 16:27:27 +0400

    Сделано задание 2, добавлены все необходимые классы и тесты

---
+++ src/main/java/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,69 @@
+package functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction
+{
+    //Метод получения количества табулированных значений
+    public abstract int getCount();
+
+    //Метод, получающий значение аргумента x по номеру индекса
+    public abstract double getX(int index);
+
+    //Метод, получающий значение y по номеру индекса
+    public abstract double getY(int index);
+
+    //Метод, задающий значение y по номеру индекса
+    public abstract void setY(int index, double value);
+
+    //Метод, возвращающий индекс аргумента x
+    public abstract int indexOfX(double x);
+
+    //Метод, возвращающий индекс первого вхождения значения y. Если такого y в таблице нет, то необходимо вернуть -1
+    public abstract int indexOfY(double y);
+
+    //Метод, возвращающий самый левый x
+    public abstract double leftBound();
+
+    //Метод, возвращающий самый правый x
+    public abstract double rightBound();
+
+    protected double[] xValues;
+    protected double[] yValues;
+    protected int count;
+
+/*    protected AbstractTabulatedFunction(double[] xValues, double[] yValues)
+    {
+        this.xValues = xValues;
+        this.yValues = yValues;
+        count = xValues.length;
+    }*/
+
+    protected int floorIndexOfX(double x)
+    {
+        return 0;
+    }
+
+    protected double extrapolateLeft(double x)
+    {
+        return 0;
+    }
+
+    protected double extrapolateRight(double x)
+    {
+        return 0;
+    }
+
+    protected double interpolate(double x, int floorIndex)
+    {
+        return 0;
+    }
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY)
+    {
+        return leftY + (x - leftX) * (rightY - leftY) / (rightX - leftX);
+    }
+
+    public double apply(double x)
+    {
+        return 0;
+    }
+}
