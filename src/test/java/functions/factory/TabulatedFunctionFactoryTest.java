1025fc52e33eee77adc9406b5127c96507e5d16c
KateParf <katya.parfenova.2005@yandex.ru>
2023-11-05 11:38:12 +0400

    createStrict + createStrictUnmodifiable tests

--- src/test/java/functions/factory/TabulatedFunctionFactoryTest.java
+++ src/test/java/functions/factory/TabulatedFunctionFactoryTest.java
@@ -15,4 +15,42 @@ public class TabulatedFunctionFactoryTest {
         TabulatedFunction funkArr = factoryArr.create(new double[]{1, 2}, new double[]{1, 2});
         assertTrue(funkArr instanceof ArrayTabulatedFunction);
     }
+
+    @Test
+    void StrictFactoryTest(){
+        TabulatedFunctionFactory factoryLinked = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction funkLinked = factoryLinked.createStrict(new double[]{1, 2}, new double[]{1, 2});
+
+        TabulatedFunctionFactory factoryArr = new ArrayTabulatedFunctionFactory();
+        TabulatedFunction funkArr = factoryArr.createStrict(new double[]{1, 2}, new double[]{1, 2});
+
+
+        var res1 = assertThrows(UnsupportedOperationException.class, () -> {
+            var y = funkLinked.apply(5);
+        });
+        var res2 = assertThrows(UnsupportedOperationException.class, () -> {
+            var y =  funkArr.apply(3);
+        });
+    }
+
+    @Test
+    void StrictUnmodifFactoryTest(){
+        TabulatedFunctionFactory factoryLinked = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction funkLinked = factoryLinked.createStrictUnmodifiable(new double[]{1, 2}, new double[]{1, 2});
+
+        TabulatedFunctionFactory factoryArr = new ArrayTabulatedFunctionFactory();
+        TabulatedFunction funkArr = factoryArr.createStrictUnmodifiable(new double[]{1, 2}, new double[]{1, 2});
+
+
+        var res1 = assertThrows(UnsupportedOperationException.class, () -> {
+            var y = funkLinked.apply(5);
+        });
+
+        ///!!!! раскоментить
+        /*
+        var res2 = assertThrows(UnsupportedOperationException.class, () -> {
+            funkArr.setY(3, 6);
+        });
+         */
+    }
 }
