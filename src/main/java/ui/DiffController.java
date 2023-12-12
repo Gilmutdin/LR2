2bbae7a2120bb02195da429b6a4ff59b606a21a2
KateParf <katya.parfenova.2005@yandex.ru>
2023-12-12 17:20:27 +0400

    вынос в базовый класс общих операций с таблицами

--- src/main/java/ui/DiffController.java
+++ src/main/java/ui/DiffController.java
@@ -1,30 +1,16 @@
 package ui;
 
-
-import exceptions.InconsistentFunctionsException;
-import functions.Insertable;
-import functions.Removable;
 import functions.TabulatedFunction;
-
-import functions.factory.TabulatedFunctionFactory;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextInputDialog;
 import operations.TabulatedDifferentialOperator;
-import operations.TabulatedFunctionOperationService;
-
 import java.io.IOException;
-import java.util.Optional;
-
-import static operations.TabulatedFunctionOperationService.asPoints;
 import static ui.Window.openFuncWindow;
-import static ui.Window.openWindow;
 
-public class DiffController {
+
+public class DiffController extends BaseFuncController {
 
     @FXML
     TableView table1;
@@ -33,18 +19,7 @@ public class DiffController {
     private TabulatedFunction func1 = null;
     private TabulatedFunction func2 = null;
 
-    private void fillTable(TabulatedFunction func, TableView table){
-        ObservableList<Point> points = FXCollections.observableArrayList();
-        functions.Point[] funcPoints = asPoints(func);
-        int cnt = funcPoints.length;
-        for (int i = 0; i < cnt; i++) {
-            points.add(new ui.Point(funcPoints[i].x, funcPoints[i].y));
-        }
-        table.setItems(points);
-    }
-    protected boolean checkFuncNotNull(TabulatedFunction fu){
-        return (fu != null);
-    }
+
     @FXML
     protected void onCreateMathFuncButtonClick(){
         var func = openFuncWindow(400, 200, "Конструктор математических функций", "CreateByFuncView.fxml");
@@ -71,7 +46,9 @@ public class DiffController {
             fillTable(func1, table1);
         }
     }
+
     // -----сохранение функций
+
     @FXML
     protected void onSaveButton1Click() throws IOException {
         if (!checkFuncNotNull(func1)){
@@ -93,117 +70,38 @@ public class DiffController {
 
 
     //---- редактирование
-    public void colEdit(TableColumn.CellEditEvent<ui.Point, String> evt) {
-        String val = evt.getNewValue();
-        var tblView = evt.getTableView();
-        var idx = evt.getTablePosition().getRow();
-        ui.Point point = (ui.Point) tblView.getItems().get(idx);
 
-        boolean res = point.setYstr(val);
-        if (! res)
-            tblView.refresh();
-        else
-            func1.setY(idx, point.getY());
-        tblView.requestFocus();
+    public void colEdit(TableColumn.CellEditEvent<ui.Point, String> evt) {
+        СolEdit(evt, func1);
     }
 
     public void onAddPointToFuncButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func1)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-        var tblView = table1;
-
-        // получить тек строку
-        table1.requestFocus();
-        var idx = table1.getFocusModel().getFocusedCell().getRow();
-
-        // Хнов = Хтек + Хтек+1 / 2  или просто Хтек +1 если послед
-        double Xnew = (idx < func1.getCount() - 1 ) ? (func1.getX(idx) + func1.getX(idx+1))/2 : func1.getX(idx) + 1;
-
-        //--- показать стд окошо ввода 1 числа, заполнить по умол
-        TextInputDialog dialog = new TextInputDialog("");
-        dialog.setTitle("Добавить новое значение X");
-        dialog.setHeaderText("Добавьте новое значение X. \nЗначение Y отредактируйте в таблице самостоятельно.");
-        dialog.setContentText("Введите X:");
-
-
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()){
-            try {
-                Xnew = Double.parseDouble(result.get());
-            }
-            catch (Exception ex){
-                Window.showAlert("Ввели не число");
-                return;
-            }
-        }
-        // Унов = упред
-        double Ynew = func1.getY(idx);
-
-        // добавляем в табл
-        ((Insertable)func1).insert(Xnew, Ynew);
-
-        //// добавить в итемсы после тек
-        tblView.getItems().add(idx+1, new functions.Point(Xnew, Ynew));
-        // пересчет итемсы (врем)
-        fillTable(func1, table1);
-
-        table1.getSelectionModel().select(idx+1);
-        table1.getFocusModel().focus(idx+1);
+        AddPointToFunc(table1, func1);
     }
 
     public void onDelPointFromFuncButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func1)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-        // не удалять когда 2
-        if (func1.getCount() <= 2) {
-            Window.showAlert("Значений в таблице не должно быть меньше 2х");
-            return;
-        }
-
-        var tblView = table1;
-        // получить тек строку
-        table1.requestFocus();
-        var idx = table1.getFocusModel().getFocusedCell().getRow();
-
-        // удалить из итемсов
-        tblView.getItems().remove(idx);
-        // удалить из табл
-        ((Removable)func1).remove(idx);
+        DelPointFromFunc(table1, func1);
     }
 
-    private interface BiOperation
-    {
-        TabulatedFunction apply(TabulatedFunction f1);
-    }
 
-    private void doOperation(BiOperation operation)
+    @FXML
+    protected void onDiffButtonClick()
     {
         // если функция не задана
         if (!checkFuncNotNull(func1))
         {
-            Window.showAlert("Вы не задали функцию 1");
+            Window.showAlert("Вы не задали исходную функцию");
             return;
         }
 
         try
         {
-            func2 = operation.apply(func1);
+            func2 = new TabulatedDifferentialOperator(Settings.factory).derive(func1);
             fillTable(func2, table2);
         }
         catch (Exception ex)
         {
             Window.showAlert(ex.getMessage());
         }
-    }
-    @FXML
-    protected void onDiffButtonClick()
-    {
-        doOperation((f1) -> new TabulatedDifferentialOperator(Settings.factory).derive(f1));
     }
 }
--- src/main/java/ui/OperationsController.java
+++ src/main/java/ui/OperationsController.java
@@ -1,29 +1,16 @@
 package ui;
 
 import functions.*;
-import functions.Point;
-import functions.factory.LinkedListTabulatedFunctionFactory;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import operations.TabulatedFunctionOperationService;
-
 import java.io.*;
-import java.util.Optional;
-
-import static io.FunctionsIO.serializeJson;
-import static operations.TabulatedFunctionOperationService.asPoints;
 import static ui.Window.openFuncWindow;
 
-public class OperationsController {
+
+public class OperationsController extends BaseFuncController {
 
     private TabulatedFunction func1 = null;
     private TabulatedFunction func2 = null;
@@ -36,21 +23,6 @@ public class OperationsController {
     TableView table3;
 
     //----
-    private void fillTable(TabulatedFunction func, TableView table){
-            ObservableList<ui.Point> points = FXCollections.observableArrayList();
-            Point[] funcPoints = asPoints(func);
-            int cnt = funcPoints.length;
-            for (int i = 0; i < cnt; i++) {
-                points.add(new ui.Point(funcPoints[i].x, funcPoints[i].y));
-            }
-            table.setItems(points);
-    }
-
-    // -----задание функций
-
-    protected boolean checkFuncNotNull(TabulatedFunction fu){
-        return (fu != null);
-    }
 
     @FXML
     protected void onCreateMathFunc1ButtonClick(){
@@ -186,167 +158,24 @@ public class OperationsController {
     //---- редактирование
 
     public void colEdit(TableColumn.CellEditEvent<ui.Point, String> evt) {
-        String val = evt.getNewValue();
         var tblView = evt.getTableView();
-        var idx = evt.getTablePosition().getRow();
-        ui.Point point = (ui.Point) tblView.getItems().get(idx);
-
-        boolean res = point.setYstr(val);
-        if (! res)
-            tblView.refresh();
-        else
-            ((tblView == table1) ? func1 : func2).setY(idx, point.getY());
-        tblView.requestFocus();
+        СolEdit(evt, ((tblView == table1) ? func1 : func2));
     }
 
     //...
     public void onAddPointToFunc1ButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func1)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-
-        var tblView = table1;
-
-        // получить тек строку
-        table1.requestFocus();
-        var idx = table1.getFocusModel().getFocusedCell().getRow();
-
-        // Хнов = Хтек + Хтек+1 / 2  или просто Хтек +1 если послед
-        double Xnew = 0;
-
-        //--- показать стд окошо ввода 1 числа, заполнить по умол
-        TextInputDialog dialog = new TextInputDialog("");
-        dialog.setTitle("Добавить новое значение X");
-        dialog.setHeaderText("Добавьте новое значение X. \nЗначение Y отредактируйте в таблице самостоятельно.");
-        dialog.setContentText("Введите X:");
-        Image image = new Image((Window.class.getResource("img/manual.png").toExternalForm()));
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(48);
-        imageView.setFitWidth(48);
-        dialog.setGraphic(imageView);
-
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()){
-            try {
-                Xnew = Double.parseDouble(result.get());
-            }
-            catch (Exception ex){
-                Window.showAlert("Ввели не число");
-                return;
-            }
-        }
-        else return;
-        // Унов = упред
-        double Ynew = func1.getY(idx);
-
-        // добавляем в табл
-        ((Insertable)func1).insert(Xnew, Ynew);
-
-        //// добавить в итемсы после тек
-        tblView.getItems().add(idx+1, new Point(Xnew, Ynew));
-        // пересчет итемсы (врем)
-        fillTable(func1, table1);
-
-        table1.getSelectionModel().select(idx+1);
-        table1.getFocusModel().focus(idx+1);
+        AddPointToFunc(table1, func1);
     }
 
     public void onDelPointFromFunc1ButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func1)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-        // не удалять когда 2
-        if (func1.getCount() <= 2) {
-            Window.showAlert("Значений в таблице не должно быть меньше 2х");
-            return;
-        }
-
-        var tblView = table1;
-        // получить тек строку
-        table1.requestFocus();
-        var idx = table1.getFocusModel().getFocusedCell().getRow();
-
-        // удалить из итемсов
-        tblView.getItems().remove(idx);
-        // удалить из табл
-        ((Removable)func1).remove(idx);
+        DelPointFromFunc(table1, func1);
     }
 
     public void onAddPointToFunc2ButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func2)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-        var tblView = table2;
-
-        // получить тек строку
-        table2.requestFocus();
-        var idx = table2.getFocusModel().getFocusedCell().getRow();
-
-        // Хнов = Хтек + Хтек+1 / 2  или просто Хтек +1 если послед
-        double Xnew = (idx < func2.getCount() - 1 ) ? (func2.getX(idx) + func2.getX(idx+1))/2 : func2.getX(idx) + 1;
-
-        //--- показать стд окошо ввода 1 числа, заполнить по умол
-        TextInputDialog dialog = new TextInputDialog("");
-        dialog.setTitle("Добавить новое значение X");
-        dialog.setHeaderText("Добавьте новое значение X. \nЗначение Y отредактируйте в таблице самостоятельно.");
-        dialog.setContentText("Введите X:");
-        Image image = new Image((Window.class.getResource("img/manual.png").toExternalForm()));
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(48);
-        imageView.setFitWidth(48);
-        dialog.setGraphic(imageView);
-
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()){
-            try {
-                Xnew = Double.parseDouble(result.get());
-            }
-            catch (Exception ex){
-                Window.showAlert("Ввели не число");
-                return;
-            }
-        }
-        // Унов = упред
-        double Ynew = func2.getY(idx);
-
-        // добавляем в табл
-        ((Insertable)func2).insert(Xnew, Ynew);
-
-        //// добавить в итемсы после тек
-        tblView.getItems().add(idx+1, new Point(Xnew, Ynew));
-        // пересчет итемсы (врем)
-        fillTable(func2, table2);
-
-        table2.getSelectionModel().select(idx+1);
-        table2.getFocusModel().focus(idx+1);
+        AddPointToFunc(table2, func2);
     }
 
     public void onDelPointFromFunc2ButtonClick(ActionEvent actionEvent) {
-        if (!checkFuncNotNull(func2)){
-            //если функции не заданы
-            Window.showAlert("Вы не задали функцию");
-            return;
-        }
-        // не удалять когда 2
-        if (func2.getCount() <= 2) {
-            Window.showAlert("Значений в таблице не должно быть меньше 2х");
-            return;
-        }
-
-        var tblView = table2;
-        // получить тек строку
-        table2.requestFocus();
-        var idx = table2.getFocusModel().getFocusedCell().getRow();
-
-        // удалить из итемсов
-        tblView.getItems().remove(idx);
-        // удалить из табл
-        ((Removable)func2).remove(idx);
+        DelPointFromFunc(table2, func2);
     }
 }
